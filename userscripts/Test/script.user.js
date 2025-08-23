bbf9bdec3a1c2d1eee5be042a93df940446e9d57
Kurotaku <54163348+Kurotaku-sama@users.noreply.github.com>
2025-08-23 15:22:25 +0200

    Changed library url

--- userscripts/Test/script.user.js
+++ userscripts/Test/script.user.js
@@ -9,7 +9,7 @@
 // @icon         https://static.twitchcdn.net/assets/favicon-32-e29e246c157142c94346.png
 // @updateURL    https://raw.githubusercontent.com/Kurotaku-sama/Userscripts/main/userscripts/Test/script.user.js
 // @downloadURL  https://raw.githubusercontent.com/Kurotaku-sama/Userscripts/main/userscripts/Test/script.user.js
-// @require      https://raw.githubusercontent.com/Kurotaku-sama/Userscripts/main/userscripts/libraries/kuros_library.js
+// @require      https://raw.githubusercontent.com/Kurotaku-sama/Userscripts/main/libraries/kuros_library.js
 // @require      https://cdn.jsdelivr.net/npm/sweetalert2
 // @grant        GM_registerMenuCommand
 // ==/UserScript==
