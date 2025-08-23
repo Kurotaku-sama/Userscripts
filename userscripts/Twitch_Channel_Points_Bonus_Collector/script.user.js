6528081bcbaf6f24de1e746f65c38bc8427cc0bb
Kurotaku <54163348+Kurotaku-sama@users.noreply.github.com>
2025-08-23 12:45:33 +0200

    Url change

--- userscripts/Twitch_Channel_Points_Bonus_Collector/script.user.js
+++ userscripts/Twitch_Channel_Points_Bonus_Collector/script.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Twitch Channel Points Bonus Collector
 // @namespace    https://kurotaku.de
-// @version      0.9
+// @version      0.9.1
 // @description  Automatically collects Twitch channel points bonuses
 // @author       Kurotaku
 // @license      CC BY-NC-SA 4.0
 // @match        https://www.twitch.tv/*
 // @icon         https://static.twitchcdn.net/assets/favicon-32-e29e246c157142c94346.png
-// @updateURL    https://github.com/Kurotaku-sama/Userscripts/raw/main/Twitch_Channel_Points_Bonus_Collector/script.user.js
-// @downloadURL  https://github.com/Kurotaku-sama/Userscripts/raw/main/Twitch_Channel_Points_Bonus_Collector/script.user.js
+// @updateURL    https://raw.githubusercontent.com/Kurotaku-sama/Userscripts/main/userscripts/Twitch_Channel_Points_Bonus_Collector/script.user.js
+// @downloadURL  https://raw.githubusercontent.com/Kurotaku-sama/Userscripts/main/userscripts/Twitch_Channel_Points_Bonus_Collector/script.user.js
 // @require      https://gist.github.com/Kurotaku-sama/1a7dcb227ce3d7a1b596afe725c0052a/raw/kuros_library.js
 // @require      https://cdn.jsdelivr.net/npm/sweetalert2
 // @grant        GM_registerMenuCommand
