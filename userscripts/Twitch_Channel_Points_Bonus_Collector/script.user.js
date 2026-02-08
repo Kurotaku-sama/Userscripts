9833cddfcf09c2499a01efb0ce10491d838c883b
Kurotaku <54163348+Kurotaku-sama@users.noreply.github.com>
2026-02-08 13:45:28 +0100

    Revert "."
    This reverts commit a1077dabc621aeb4e36a9d3dc3bfddeaa093b864.

--- userscripts/Twitch_Channel_Points_Bonus_Collector/script.user.js
+++ userscripts/Twitch_Channel_Points_Bonus_Collector/script.user.js
@@ -10,7 +10,7 @@
 // @updateURL       https://raw.githubusercontent.com/Kurotaku-sama/Userscripts/main/userscripts/Twitch_Channel_Points_Bonus_Collector/script.user.js
 // @downloadURL     https://raw.githubusercontent.com/Kurotaku-sama/Userscripts/main/userscripts/Twitch_Channel_Points_Bonus_Collector/script.user.js
 // @require         https://cdn.jsdelivr.net/gh/Kurotaku-sama/Userscripts@main/libraries/kuros_library.js
-// @require         https://cdn.jsdelivr.net/npm/sweetalert2@11
+// @require         cdn.jsdelivr.net/npm/sweetalert2@11
 // @require         https://openuserjs.org/src/libs/sizzle/GM_config.js
 // @grant           GM_registerMenuCommand
 // @run-at          document-body
